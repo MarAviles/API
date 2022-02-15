6fe65c30f2bad8e3de4c9ecb32729b0b53382744
unknown <mariana.avilesmartinez@gmail.com.com>
2022-02-15 09:11:31 -0600

    API

---
+++ src/test/java/com/example/practica/API/PracticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
